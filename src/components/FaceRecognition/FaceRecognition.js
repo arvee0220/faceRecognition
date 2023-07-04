b108a3192972529633a4ba41c9c872aa6eec543a
Rowell Verdejo <rowellverdejo@gmail.com>
2023-07-05 02:52:21 +0800

    push minor changes

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,19 +1,13 @@
-import React from "react";
-import "./FaceRecognition.css";
+import React from 'react';
+import './FaceRecognition.css';
 
 const FaceRecognition = ({ imageUrl, box }) => {
     return (
-        <div className="center ma">
-            <div className="absolute mt2">
-                <img
-                    id="inputimage"
-                    alt=""
-                    src={imageUrl}
-                    width="500px"
-                    heigh="auto"
-                />
+        <div className='center ma'>
+            <div className='absolute mt2'>
+                <img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto' />
                 <div
-                    className="bounding-box"
+                    className='bounding-box'
                     style={{
                         top: box.topRow,
                         right: box.rightCol,
